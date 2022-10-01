dbdc5cfca8db135e08624863dc07349b6cde4503
Diego Fernando Bulla Poveda <48418033+dibullap@users.noreply.github.com>
2022-10-01 03:42:58 -0500

    Cambiado el entorno virtual por pipenv, templates basicos creados pero falta pulirlos y saber como funcionan, ademas ya se creó la base de datos del proyecto pero falta crear las tablas diseñando el esquema de datos

--- run.py
+++ run.py
@@ -1,6 +1,31 @@
+from distutils.log import debug
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "%sqlite://database/au_bon_pain.db"
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+menu = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(menu))
+
+"""
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
+
+"""
